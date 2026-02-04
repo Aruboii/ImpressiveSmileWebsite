2f02fe086189cf2422193a922ac37268c6ae0aa7
Aryan Singh <150801384+Aruboii@users.noreply.github.com>
2026-02-04 14:26:02 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,25 +6,21 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
-const BASE_PATH = "/ImpressiveSmileWebsite";
-
-function AppRouter() {
+function Router() {
   return (
     <Switch>
-      <Route path={`${BASE_PATH}/`} component={Home} />
+      <Route path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
 }
 
-export default function App(): JSX.Element {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <WouterRouter base={BASE_PATH}>
-          <AppRouter />
-        </WouterRouter>
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
